3f2e327a4b5d997726cf10f7a268bef0ca1bdf71
wjosephmark <wjosephmark@gmail.com>
2020-09-24 12:05:46 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Kilee's website is going to be so fucking amazing!!!</h1>
+      </div>
+    );
+  }
+}

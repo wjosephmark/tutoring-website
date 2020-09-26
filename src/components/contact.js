38d2c92f062ed951a23ca1f66d562793d7bae04f
wjosephmark <wjosephmark@gmail.com>
2020-09-25 21:09:27 -0600

    added icon and changed email on contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,11 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default function Contact() {
+
+    const handleImgClick = () => {
+        return null
+    }
+
     return (
         <div className="app">
             <div className="email-wrapper">
-                <FontAwesomeIcon icon="envelope"/>
-                <a href="mailto:tutoringbykilee@gmail.com">Email me</a>
+                {/* <FontAwesomeIcon icon="envelope"/> */}
+                <img 
+                    className="mail-image"
+                    src="https://www.iconfinder.com/data/icons/neon-line-social-circles-black/100/Neon_Line_Social_Circles_50Icon_10px_grid-51-512.png"
+                    onClick={() => handleImgClick()}
+                />
+                <a href="mailto:tutoringbykilee@gmail.com">tutoringbykilee@gmail.com</a>
             </div>
         </div>
     );

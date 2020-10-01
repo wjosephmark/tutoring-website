211cdc32b9fb1f683c88218309736d881af64c24
wjosephmark <wjosephmark@gmail.com>
2020-10-01 16:20:22 -0600

    initial heroku commit

--- src/components/availability.js
+++ src/components/availability.js
@@ -2,15 +2,39 @@ import React, { useState } from "react";
 
 export default function Availability() {
   
-  const [monday, setMonday] = useState("")
-  const [tuesday , setTuesday] = useState("")
-  const [wednesday, setWednesday] = useState("")
-  const [thursday, setThursday] = useState("")
-  const [friday, setFriday] = useState("")
+  const [monday, setMonday] = useState("9 a.m. - 5 p.m.")
+  const [tuesday , setTuesday] = useState("9 a.m. - 5 p.m.")
+  const [wednesday, setWednesday] = useState("9 a.m. - 5 p.m.")
+  const [thursday, setThursday] = useState("9 a.m. - 5 p.m.")
+  const [friday, setFriday] = useState("9 a.m. - 5 p.m.")
 
   return (
-    <div className="app">
-      <h1>Hello from availability</h1>
+    <div className="availability-app">
+      <div className="header-wrapper">
+        <h1>Availability</h1>
+      </div>
+      <div className="days-wrapper">
+        <div className="hours-wrapper">
+          <p>Monday: {monday}</p>
+        </div>   
+
+        <div className="hours-wrapper">
+          <p>Tuesday: {tuesday}</p>
+        </div>  
+
+        <div className="hours-wrapper">
+          <p>Wednesday: {wednesday}</p>
+        </div>  
+
+        <div className="hours-wrapper">
+          <p>Thursday: {thursday}</p>
+        </div>  
+
+        <div className="hours-wrapper">
+          <p>Friday: {friday}</p>
+        </div>   
+
+      </div>
     </div>
   );
 }

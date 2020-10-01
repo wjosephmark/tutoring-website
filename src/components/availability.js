2e0590660776d496a0b63c9b549fe9b56e33261d
wjosephmark <wjosephmark@gmail.com>
2020-10-01 16:07:58 -0600

    added state to availability

--- src/components/availability.js
+++ src/components/availability.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 
 export default function Availability() {
+  
+  const [monday, setMonday] = useState("")
+  const [tuesday , setTuesday] = useState("")
+  const [wednesday, setWednesday] = useState("")
+  const [thursday, setThursday] = useState("")
+  const [friday, setFriday] = useState("")
+
   return (
     <div className="app">
       <h1>Hello from availability</h1>

1edc16a3796bbf1b459b7f4bc13bb51d2ade10ec
wjosephmark <wjosephmark@gmail.com>
2020-10-02 14:33:03 -0600

    finished adding phone to contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -15,31 +15,33 @@ export default function Contact() {
 
     return (
         <div className="contact-app">
-            <div className="email-wrapper">
-                <img 
-                    className="mail-image"
-                    src="https://www.iconfinder.com/data/icons/neon-line-social-circles-black/100/Neon_Line_Social_Circles_50Icon_10px_grid-51-512.png"
-                    onClick={() => handleImgClick()}
-                />
-                <div className="email-link-wrapper">
-                    <a 
-                        href="mailto:tutoringbykilee@gmail.com"
-                        id="email-link"
-                    >tutoringbykilee@gmail.com</a>
+            <div className="contact-link-wrapper">
+                <div className="email-wrapper">
+                    <img 
+                        className="mail-image"
+                        src="https://www.iconfinder.com/data/icons/neon-line-social-circles-black/100/Neon_Line_Social_Circles_50Icon_10px_grid-51-512.png"
+                        onClick={() => handleImgClick()}
+                    />
+                    <div className="email-link-wrapper">
+                        <a 
+                            href="mailto:tutoringbykilee@gmail.com"
+                            id="email-link"
+                        >tutoringbykilee@gmail.com</a>
+                    </div>
                 </div>
-            </div>
 
-            <div className="phone-wrapper">
-                <img 
-                    className="mail-image"
-                    src="https://i.ibb.co/xY49wSL/phone-icon.jpg"
-                    onClick={() => handleImgClick()}
-                />
-                <div className="phone-link-wrapper">
-                    <a 
-                        href="tel: 406-799-2817"
-                        id="phone-link"
-                    >(406) 799-2817</a>
+                <div className="phone-wrapper">
+                    <img 
+                        className="mail-image"
+                        src="https://i.ibb.co/xY49wSL/phone-icon.jpg"
+                        onClick={() => handleImgClick()}
+                    />
+                    <div className="phone-link-wrapper">
+                        <a 
+                            href="tel: 406-799-2817"
+                            id="phone-link"
+                        >(406) 799-2817</a>
+                    </div>
                 </div>
             </div>
         </div>

64e7cd3d67bf3e455e76c9c79e105def9011c75f
wjosephmark <wjosephmark@gmail.com>
2020-10-06 10:20:19 -0600

    changed app to grid layout

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="app">
       <div className="grid-wrapper">
-        <div className="first-row-wrapper">
+        {/* <div className="first-row-wrapper"> */}
           <div className="first-text-wrapper">
             <p className="first-text">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at leo rhoncus, eleifend urna sit amet, feugiat erat. Donec ultricies finibus nunc, vitae viverra mi pharetra eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec pellentesque, purus tempor aliquet congue, enim mauris dapibus mi, nec ullamcorper nisi risus id mi. In molestie porta sodales. Aliquam id purus lacus. Curabitur consequat ante cursus blandit pellentesque. Quisque consectetur, odio sed pretium ullamcorper, lectus erat laoreet ante, mattis efficitur est magna id arcu. Aliquam nunc neque, pharetra at sagittis ac, imperdiet quis neque. Maecenas commodo magna eu rutrum euismod. Nullam fringilla quam augue, in elementum nisl suscipit nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
@@ -18,10 +18,10 @@ export default function App() {
             <img src="https://i.ibb.co/Cm142x0/mathimage.jpg" />
           </div>
           
-        </div>
+        {/* </div> */}
 
       
-        <div className="second-row-wrapper">
+        {/* <div className="second-row-wrapper"> */}
           <div className="second-image-wrapper">
             <img src="https://i.ibb.co/jMstMqQ/kid-doing-math.png" />
           </div>
@@ -31,7 +31,7 @@ export default function App() {
             </p>
           </div>
 
-        </div>
+        {/* </div> */}
       </div>
     </div>
   );

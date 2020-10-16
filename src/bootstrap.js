ceb8030e54105b2e80ff0446e31ab3e03d616c85
wjosephmark <wjosephmark@gmail.com>
2020-10-16 10:48:00 -0600

    added account page when logged in

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,6 +6,7 @@ import App from "./components/app";
 import Availability from "./components/availability"
 import Contact from "./components/contact"
 import Auth from "./components/auth"
+import Account from "./components/account"
 
 import "./style/main.scss"
 
@@ -15,6 +16,7 @@ function Main() {
   const [availability, setAvailability] = useState("nav-link")
   const [contact, setContact] = useState("nav-link")
   const [login, setLogin] = useState("nav-link")
+  const [account, setAccount] = useState("nav-link")
   const [navbarClass, setNavbarClass] = useState("navbar-wrapper")
   const [loggedInStatus, setLoggedInStatus] = useState(false)
   
@@ -22,13 +24,15 @@ function Main() {
     "/": () => <App home={home} setHome={setHome} handleHomeClick={handleHomeClick} />,
     "/contact": () => <Contact handleContactClick={handleContactClick} />,
     "/availability": () => <Availability handleAvailabilityClick={handleAvailabilityClick}  loggedInStatus={loggedInStatus}/>,
-    "/login": () => <Auth  handleLoginClick={handleLoginClick} />
+    "/login": () => <Auth  handleLoginClick={handleLoginClick} />,
+    "/account": () => <Account  handleAccountClick={handleAccountClick} />
   }
   const handleHomeClick = () => {
     setHome("nav-link-active")
     setAvailability("nav-link")
     setContact("nav-link")
     setLogin("nav-link")
+    setAccount("nav-link")
   }
   
   const handleAvailabilityClick = () => {
@@ -36,6 +40,7 @@ function Main() {
     setAvailability("nav-link-active")
     setContact("nav-link")
     setLogin("nav-link")
+    setAccount("nav-link")
   }
 
   const handleContactClick = () => {
@@ -43,6 +48,7 @@ function Main() {
     setAvailability("nav-link")
     setContact("nav-link-active")
     setLogin("nav-link")
+    setAccount("nav-link")
   }
 
   const handleLoginClick = () => {
@@ -52,35 +58,75 @@ function Main() {
     setLogin("nav-link-active")
   }
 
-  return(
-    <div>
-      <div className={navbarClass}>
-        <div className="logo">
-          <div className="black-text">
-            <h1>Tutoring By</h1>
+  const handleAccountClick = () => {
+    setHome("nav-link")
+    setAvailability("nav-link")
+    setContact("nav-link")
+    setLogin("nav-link")
+    setAccount("nav-link-active")
+  }
+
+  if(loggedInStatus == false){
+    return(
+      <div>
+        <div className={navbarClass}>
+          <div className="logo">
+            <div className="black-text">
+              <h1>Tutoring By</h1>
+            </div>
+            <div className="pink-text">
+              <h1>Kilee</h1>
+            </div>
           </div>
-          <div className="pink-text">
-            <h1>Kilee</h1>
+          <div className="navbar">
+            <div className="nav-link-wrapper">
+              <A className={home} onClick={() => handleHomeClick()} href="/">Home</A>
+            </div>
+            <div className="nav-link-wrapper">
+              <A className={availability} onClick={() => handleAvailabilityClick()} href="/availability">Availability</A>
+            </div>
+            <div className="nav-link-wrapper">
+              <A className={contact} onClick={() => handleContactClick()} href="/contact">Contact</A>
+            </div>
+            <div className="nav-link-wrapper">
+              <A className={login} onClick={() => handleLoginClick()} href="/login">Login</A>
+            </div>
           </div>
         </div>
-        <div className="navbar">
-          <div className="nav-link-wrapper">
-            <A className={home} onClick={() => handleHomeClick()} href="/">Home</A>
-          </div>
-          <div className="nav-link-wrapper">
-            <A className={availability} onClick={() => handleAvailabilityClick()} href="/availability">Availability</A>
-          </div>
-          <div className="nav-link-wrapper">
-            <A className={contact} onClick={() => handleContactClick()} href="/contact">Contact</A>
-          </div>
-          <div className="nav-link-wrapper">
-            <A className={login} onClick={() => handleLoginClick()} href="/login">Login</A>
-          </div>
-        </div>
+        {useRoutes(routes)}
       </div>
-      {useRoutes(routes)}
-    </div>
-  )
+    )
+  } else if(loggedInStatus == true) {
+    return(
+      <div>
+        <div className={navbarClass}>
+          <div className="logo">
+            <div className="black-text">
+              <h1>Tutoring By</h1>
+            </div>
+            <div className="pink-text">
+              <h1>Kilee</h1>
+            </div>
+          </div>
+          <div className="navbar">
+            <div className="nav-link-wrapper">
+              <A className={home} onClick={() => handleHomeClick()} href="/">Home</A>
+            </div>
+            <div className="nav-link-wrapper">
+              <A className={availability} onClick={() => handleAvailabilityClick()} href="/availability">Availability</A>
+            </div>
+            <div className="nav-link-wrapper">
+              <A className={contact} onClick={() => handleContactClick()} href="/contact">Contact</A>
+            </div>
+            <div className="nav-link-wrapper">
+              <A className={account} onClick={() => handleAccountClick()} href="/account">Account</A>
+            </div>
+          </div>
+        </div>
+        {useRoutes(routes)}
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<Main />, document.querySelector(".app-wrapper"))
---
+++ src/components/account.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from 'react'
+
+export default function Account() {
+    return(
+        <h1>Hello from Account</h1>
+    )
+}

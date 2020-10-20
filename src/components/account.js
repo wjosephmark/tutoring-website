96d6f1187f5fbfc3a6c19ba7e6ac50e8bb7a89cf
wjosephmark <wjosephmark@gmail.com>
2020-10-20 17:30:31 -0600

    changes to patch method on account

--- src/components/account.js
+++ src/components/account.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {navigate} from 'hookrouter'
+import {axios} from "axios"
 
 export default function Account(props) {
     const [edit, setEdit] = useState(false)
@@ -17,35 +18,53 @@ export default function Account(props) {
         navigate("/")
     }
 
+    const handleSubmit = () => {
+        axios.patch(
+            "http://localhost:5000/users",
+            {
+                first_name: newFirstName,
+                last_name: newLastName,
+                email: newEmail,
+            },
+          )
+          .then(response => {
+            console.log(response)
+          })
+          .catch(error => {
+            console.log("An error occured", error)
+        });
+    }
+
     if(props.loggedInStatus == true){
         if(edit == false) {
             return(
                 <div className="account-app">
-                    <div className="user-info-wrapper">
-                        <div className="email-wrapper">
-                            <p>{email}</p>
-                        </div>
-                        <div className="name-wrapper">
-                            <p>{firstName}</p>
-                            <p>{lastName}</p>
+                        <div className="content-wrapper">
+                            <p>Email: {email}</p>
+                            <p>First name: {firstName}</p>
+                            <p>Last name: {lastName}</p>
+                            <div className="buttons-wrapper">
+                                <button onClick={() => setEdit(true)}>Edit Account</button>
+                                <button onClick={() => handleLogoutClick()}>Logout</button>
+                            </div>
                         </div>
                     </div>
-                    <div className="buttons-wrapper">
-                        <button onClick={() => setEdit(true)}>Edit Account</button>
-                        <button onClick={() => handleLogoutClick()}>Logout</button>
-                    </div>
-                </div>
             )
         } else {
             return(
-                <form className="account-app">
-                    <div className="user-input-wrapper">
-                        <div className="email-input-wrapper">
-                            <input type="text" value={email} onChange={(e) => setNewEmail(e.target.value)} />
+                <form className="account-app" onSubmit={() => handleSubmit()}>
+                    <div className="inputs-wrapper">
+                        <div className="input-wrapper">
+                            {/* <p>Email: </p> */}
+                            <input type="text" value={email} onChange={e => setNewEmail(e.target.value)} />
                         </div>
-                        <div className="name-input-wrapper">
-                            <input type="text" value={firstName} onChange={(e) => setNewFirstName(e.target.value)} />
-                            <input type="text" value={lastName} onChange={(e) => setNewLastName(e.target.value)} />
+                        <div className="input-wrapper">
+                            <p>First name: </p>
+                            <input type="text" value={firstName} onChange={e => setNewFirstName(e.target.value)} />
+                        </div>
+                        <div className="input-wrapper">
+                            <p>Last name: </p>
+                            <input type="text" value={lastName} onChange={e => setNewLastName(e.target.value)} />
                         </div>
                     </div>
                     <div className="buttons-wrapper">
